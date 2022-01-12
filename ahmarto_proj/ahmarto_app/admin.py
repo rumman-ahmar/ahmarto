f277b565af54486c6e690affe3de02e72d86300f
Rumman Ahmar <rummanahmar@gmail.com>
2022-01-12 13:03:35 +0530

    changes in model and admin file

--- ahmarto_proj/ahmarto_app/admin.py
+++ ahmarto_proj/ahmarto_app/admin.py
@@ -1,14 +1,80 @@
+import csv
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Customer)
+admin.site.register(Address)
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductAttributeBase)
-admin.site.register(ProductAttribute)
 admin.site.register(Cart)
-admin.site.register(Order)
 admin.site.register(ReturnedOrder)
 
 
-# Register your models here.
+######################
+# Products
+######################
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+class ProductAttributeInline(admin.TabularInline):
+    model = ProductAttribute
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'description', 'discount_price', 'sell_price', 'category',
+        'created_at', 'updated_at', 'is_active'
+    ]
+
+    inlines = [ProductAttributeInline, ProductImageInline]
+admin.site.register(Product, ProductAdmin)
+
+
+######################
+# Customer
+######################
+class AddressInline(admin.TabularInline):
+    model = Address
+    extra = 0
+
+
+class CartInline(admin.TabularInline):
+    model = Cart
+    extra = 0
+
+
+class OrderInline(admin.TabularInline):
+    model = Order
+    extra = 0
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = [AddressInline, CartInline, OrderInline]
+    list_display = [
+        "id", "customer", "gender", "created_at", "is_active"
+    ]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+######################
+# orders
+######################
+
+class OrderAdmin(admin.ModelAdmin):
+    # actions = ['download_csv']
+    list_display = [
+        'id', 'customer', 'product', 'quantity', 'ordered_date', 'order_status',
+        'payment_mode', 'transaction_id', 'is_returned', 'is_delivered', 'updated_at'
+        ]
+admin.site.register(Order, OrderAdmin)
--- ahmarto_proj/ahmarto_app/models.py
+++ ahmarto_proj/ahmarto_app/models.py
@@ -30,11 +30,37 @@ PAYMENT_MODE_CHOICE = (
 )
 
 
+ADDRESS_TYPE_CHOICE = (
+    ("H", "Home"),
+    ("O", "Office")
+)
+
+
 # Create your models here.
 class Customer(models.Model):
     customer = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.CharField(choices=GENDER_CHOICE, max_length=1, default=None, blank=True, null=True)
     profile_image = models.ImageField(upload_to="customer_images", blank=True, null=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Address(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    house_number = models.CharField(max_length=50, blank=False, null=False)
+    street_area = models.CharField(max_length=100, blank=False, null=False)
+    city = models.CharField(max_length=50, blank=False, null=False)
+    landmark = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=50, blank=False, null=False)
+    country = models.CharField(max_length=50, blank=False, null=False)
+    pincode = models.CharField(max_length=6, blank=False, null=False)
+    phone_number = models.CharField(max_length=10, blank=False, null=False)
+    address_type = models.CharField(choices=ADDRESS_TYPE_CHOICE, max_length=1, blank=False, null=False)
+    is_default = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
@@ -60,29 +86,22 @@ class Product(models.Model):
     discount_price = models.FloatField()
     sell_price = models.FloatField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    product_image = models.ImageField(upload_to="product_images", blank=True, null=True, default="")
+    # product_image = models.ImageField(upload_to="product_images", blank=True, null=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.id)
-
-
-class ProductAttributeBase(models.Model):
-    attribute_name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return str(self.id)
+        # data = {"id": self.id, "name": self.name}
+        # return str(data)
+        return self.name
 
 
 class ProductAttribute(models.Model):
-    attribute_name = models.ForeignKey(ProductAttributeBase, related_name='attributes', blank=True, null=True, on_delete=models.CASCADE)
-    attribute_value = models.CharField(max_length=255)
-    product = models.ForeignKey(Product, related_name='attributes', blank=True, null=True, on_delete=models.CASCADE)
+    attribute_name = models.CharField(max_length=255, blank=True, null=True)
+    attribute_value = models.CharField(max_length=255, blank=True, null=True)
+    product = models.ForeignKey(Product, related_name='product_attributes', blank=True, null=True,
+                                on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
@@ -91,11 +110,16 @@ class ProductAttribute(models.Model):
         return str(self.id)
 
 
-# class ProductAttribute(Attribute):
-#     product = models.ForeignKey(Product, related_name='attributes', blank=True, null=True, on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return str(self.id)
+class ProductImage(models.Model):
+    product_image = models.ImageField(upload_to="product_images", blank=True, null=True, default="")
+    product = models.ForeignKey(Product, related_name='product_images', blank=True, null=True,
+                                on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Cart(models.Model):
@@ -119,7 +143,7 @@ class Order(models.Model):
     payment_mode = models.CharField(choices=PAYMENT_MODE_CHOICE, max_length=10)
     transaction_id = models.CharField(max_length=255, default="", blank=True, null=True)
     is_returned = models.BooleanField(default=False)
-    is_completed = models.BooleanField(default=False)
+    is_delivered = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -134,3 +158,20 @@ class ReturnedOrder(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+# class ProductAttributeBase(models.Model):
+#     attribute_name = models.CharField(max_length=255)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     is_active = models.BooleanField(default=False)
+#
+#     def __str__(self):
+#         return str(self.id)
+
+
+# class ProductAttribute(Attribute):
+#     product = models.ForeignKey(Product, related_name='attributes', blank=True, null=True, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return str(self.id)
